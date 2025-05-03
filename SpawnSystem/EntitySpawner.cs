fb56943f0644b0a3d02e59e132552dda1daa7515
Nick Le <70725839+tecknickle@users.noreply.github.com>
2025-05-02 22:00:15 -0700

    Update EntitySpawner.cs

--- SpawnSystem/EntitySpawner.cs
+++ SpawnSystem/EntitySpawner.cs
@@ -1,20 +1,17 @@
-﻿namespace CannonMonke
+public class EntitySpawner<T> where T : Entity
 {
-    public class EntitySpawner<T> where T : Entity
+    IEntityFactory<T> entityFactory;
+    ISpawnPointStrategy spawnPointStrategy;
+
+    public EntitySpawner(IEntityFactory<T> entityFactory, ISpawnPointStrategy spawnPointStrategy)
     {
-        IEntityFactory<T> entityFactory;
-        ISpawnPointStrategy spawnPointStrategy;
-
-        public EntitySpawner(IEntityFactory<T> entityFactory, ISpawnPointStrategy spawnPointStrategy)
-        {
-            this.entityFactory = entityFactory;
-            this.spawnPointStrategy = spawnPointStrategy;
-        }
-
-        public T Spawn()
-        {
-            return entityFactory.Create(spawnPointStrategy.NextSpawnPoint());
-        }
-
+        this.entityFactory = entityFactory;
+        this.spawnPointStrategy = spawnPointStrategy;
     }
+
+    public T Spawn()
+    {
+        return entityFactory.Create(spawnPointStrategy.NextSpawnPoint());
+    }
+
 }
