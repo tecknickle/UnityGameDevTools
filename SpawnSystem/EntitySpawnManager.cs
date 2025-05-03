39cc394e6b480292c0fe3861974def2a3d67e5ff
Nick Le <70725839+tecknickle@users.noreply.github.com>
2025-05-02 21:59:56 -0700

    Update EntitySpawnManager.cs

--- SpawnSystem/EntitySpawnManager.cs
+++ SpawnSystem/EntitySpawnManager.cs
@@ -1,33 +1,30 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-namespace CannonMonke
+public abstract class EntitySpawnManager : MonoBehaviour 
 {
-    public abstract class EntitySpawnManager : MonoBehaviour 
+    [SerializeField] protected SpawnPointStrategyType spawnPointStrategyType = SpawnPointStrategyType.Linear;
+    [SerializeField] protected Transform[] spawnPoints;
+
+    protected ISpawnPointStrategy spawnPointStrategy;
+
+    protected enum SpawnPointStrategyType
     {
-        [SerializeField] protected SpawnPointStrategyType spawnPointStrategyType = SpawnPointStrategyType.Linear;
-        [SerializeField] protected Transform[] spawnPoints;
-
-        protected ISpawnPointStrategy spawnPointStrategy;
-
-        protected enum SpawnPointStrategyType
-        {
-            Linear,
-            Random
-        }
-
-        protected virtual void Awake()
-        {
-            switch (spawnPointStrategyType)
-            {
-                case SpawnPointStrategyType.Linear:
-                    spawnPointStrategy = new LinearSpawnPointStrategy(spawnPoints);
-                    break;
-                case SpawnPointStrategyType.Random:
-                    spawnPointStrategy = new RandomSpawnPointStrategy(spawnPoints);
-                    break;
-            }
-        }
-
-        public abstract void Spawn();
+        Linear,
+        Random
     }
+
+    protected virtual void Awake()
+    {
+        switch (spawnPointStrategyType)
+        {
+            case SpawnPointStrategyType.Linear:
+                spawnPointStrategy = new LinearSpawnPointStrategy(spawnPoints);
+                break;
+            case SpawnPointStrategyType.Random:
+                spawnPointStrategy = new RandomSpawnPointStrategy(spawnPoints);
+                break;
+        }
+    }
+
+    public abstract void Spawn();
 }
