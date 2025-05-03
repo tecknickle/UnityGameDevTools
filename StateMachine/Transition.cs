c713b8f624ce9d05044ca90f7863d7bf4c4a5838
Nick Le <70725839+tecknickle@users.noreply.github.com>
2025-05-02 21:58:39 -0700

    Update Transition.cs

--- StateMachine/Transition.cs
+++ StateMachine/Transition.cs
@@ -1,14 +1,11 @@
-﻿namespace CannonMonke
+public class Transition : ITransition
 {
-    public class Transition : ITransition
-    {
-        public IState To { get; }
-        public IPredicate Condition { get; }
+    public IState To { get; }
+    public IPredicate Condition { get; }
 
-        public Transition(IState to, IPredicate condition)
-        {
-            To = to;
-            Condition = condition;
-        }
+    public Transition(IState to, IPredicate condition)
+    {
+        To = to;
+        Condition = condition;
     }
 }
