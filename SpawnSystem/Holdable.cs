4b79f9e7f42f099f991dd29b46fe0a0f9b967aeb
Nick Le <70725839+tecknickle@users.noreply.github.com>
2025-05-02 22:00:32 -0700

    Update Holdable.cs

--- SpawnSystem/Holdable.cs
+++ SpawnSystem/Holdable.cs
@@ -1,69 +1,66 @@
-﻿using UnityEngine;
+using UnityEngine;
 
-namespace CannonMonke
+public class Holdable : Entity, IInteractable, IHoldable
 {
-    public class Holdable : Entity, IInteractable, IHoldable
+    [SerializeField] HoldableData holdableDataSO;
+
+    Transform holder;
+    Rigidbody rb;
+    Collider objectCollider;
+
+    void Awake()
     {
-        [SerializeField] HoldableData holdableDataSO;
+        rb = GetComponent<Rigidbody>();
+        objectCollider = GetComponent<Collider>();
+    }
 
-        Transform holder;
-        Rigidbody rb;
-        Collider objectCollider;
-
-        void Awake()
+    void FixedUpdate()
+    {
+        if (holder != null)
         {
-            rb = GetComponent<Rigidbody>();
-            objectCollider = GetComponent<Collider>();
-        }
+            // Keeps object in holding position every frame
+            holder.GetPositionAndRotation(
+                out Vector3 targetPosition, 
+                out Quaternion targetRotation);
 
-        void FixedUpdate()
+            transform.SetPositionAndRotation(targetPosition, targetRotation);
+        }
+    }
+
+    public HoldableData GetHoldableSO()
+    {
+        return holdableDataSO;
+    }
+
+    public void Interact(Transform interactor)
+    {
+        // Tells player to try to pick up this object
+        if (interactor.TryGetComponent(out PlayerObjectHolding player))
         {
-            if (holder != null)
-            {
-                // Keeps object in holding position every frame
-                holder.GetPositionAndRotation(
-                    out Vector3 targetPosition, 
-                    out Quaternion targetRotation);
-
-                transform.SetPositionAndRotation(targetPosition, targetRotation);
-            }
+            player.PickUpObject(this);
         }
+    }
 
-        public HoldableData GetHoldableSO()
-        {
-            return holdableDataSO;
-        }
+    public void Pickup(Transform newHolder)
+    {
+        // Sets player as holder of this object
+        holder = newHolder;
+        // Prevents physics from affecting object while held
+        objectCollider.enabled = false;
+        rb.isKinematic = true;
+        
+    }
 
-        public void Interact(Transform interactor)
-        {
-            // Tells player to try to pick up this object
-            if (interactor.TryGetComponent(out PlayerObjectHolding player))
-            {
-                player.PickUpObject(this);
-            }
-        }
+    public void Drop()
+    {
+        holder = null;
+        objectCollider.enabled = true;
+        rb.isKinematic = false;
+    }
 
-        public void Pickup(Transform newHolder)
-        {
-            // Sets player as holder of this object
-            holder = newHolder;
-            // Prevents physics from affecting object while held
-            objectCollider.enabled = false;
-            rb.isKinematic = true;
-            
-        }
-
-        public void Drop()
-        {
-            holder = null;
-            objectCollider.enabled = true;
-            rb.isKinematic = false;
-        }
-
-        public void Throw(Vector3 direction, float force)
-        {
-            Drop();
-            rb.AddForce(direction * force, ForceMode.Impulse);
-        }
+    public void Throw(Vector3 direction, float force)
+    {
+        Drop();
+        rb.AddForce(direction * force, ForceMode.Impulse);
     }
 }
